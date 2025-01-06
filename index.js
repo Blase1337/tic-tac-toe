453795a5086994c66c0580cc498782bf4b410cea
Blase1337 <114041435+Blase1337@users.noreply.github.com>
2025-01-06 14:04:50 -0500

    finished game logic

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
 const Gameboard = (function() {
-    let board = [" "," "," "," "," "," "," "," "," "];
-    const resetBoard = () => board = [" "," "," "," "," "," "," "," "," "];
+    let board = ["","","","","","","","",""];
+    const getBoard = () => [...board];
+    const placeMarker = (pos, marker) => {
+        if (board[pos] === "") {
+            board[pos] = marker;
+        }
+        else {
+            console.log("Please pick a valid square");
+        }
+    }
+    const resetBoard = () => board = ["","","","","","","","",""];
 
-    return {board, resetBoard};
+    return {getBoard, resetBoard, placeMarker};
 })();
 
 const Players = (function() {
-    const getPlayers = [
+    const players = [
         {
-            name: player1,
+            name: "player1",
             marker: "x"
         },
         {
-            name: player2,
+            name: "player2",
             marker: "o"
         }
     ];
-    let activePlayer = players[0];
+
+    const getPlayers = () => [...players];
+    let activePlayer = getPlayers()[0];
 
     const switchTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] :players[0] //ternary operator to switch player
@@ -27,12 +38,12 @@ const Players = (function() {
 })();
 
 function GameController() {
-    const board = Gameboard.board();
     const players = Players.getPlayers();
-    const activePlayer = Players.getActivePlayer();
+    let activePlayer = Players.getActivePlayer();
 
     const checkwin = () => {
-        const winCondition = [[0,1,2], [3,4,5], [6,7,9],[0,4,8], [6,4,2]];
+        const board = Gameboard.getBoard();
+        const winCondition = [[0,1,2], [3,4,5], [6,7,8],[0,4,8], [6,4,2], [0,3,6], [1,4,7],[2,5,8]];
         for(let combo of winCondition){
             const [a,b,c] = combo;
             if (board[a] === board[b] && board[b] === board[c]){
@@ -42,13 +53,16 @@ function GameController() {
         return null //no winner
     }
 
-    const playTurn = (pos, marker) =>{
-        board[pos] = marker;
+    const playTurn = (pos) =>{
+        const marker = activePlayer.marker;
+        Gameboard.placeMarker(pos, marker);
         const winner = checkwin(); //returns either null (no winner) or the marker of the winner
-        if (!winner) { //null would be falsy any string/value would be truthy
-            Players.switchTurn();
-            activePlayer = Players.getActivePlayer();
+        if (winner) { //null would be falsy any string/value would be truthy
+            console.log(`${winner} has won`)
+            Gameboard.resetBoard();
         }
+        Players.switchTurn();
+        activePlayer = Players.getActivePlayer();
         
     }
     return {playTurn, checkwin}
